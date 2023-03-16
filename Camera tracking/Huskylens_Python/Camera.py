a5d964968df6c76eca5beda0676cbf85f8c6cc91
RubenisR <r.rubeniszeleznovs@student.fontys.nl>
2023-03-16 16:23:36 +0100

    setting up file

--- Camera tracking/Huskylens_Python/Camera.py
+++ Camera tracking/Huskylens_Python/Camera.py
@@ -1,4 +1,5 @@
 from huskylib import HuskyLensLibrary
 
-h1 = HuskyLensLibrary("SERIAL", "/dev/ttyUSB0")
+h1 = HuskyLensLibrary("SERIAL", "/dev/ttyUSB0", 3000000)  # Serial port PC
+# hl = HuskyLensLibrary("I2C","", address=0x32,channel=0) # I2C raspeberry pi
 print(h1.knock())
