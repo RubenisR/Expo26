dd57664bfe4b35469db48c89c2c25612fce242d0
RubenisR <r.rubeniszeleznovs@student.fontys.nl>
2023-03-23 18:53:57 +0100

    Made changes to lib so it works with new smbus lib

--- Camera tracking/Huskylens_Python/Camera.py
+++ Camera tracking/Huskylens_Python/Camera.py
@@ -1,5 +1,6 @@
 from huskylib import HuskyLensLibrary
+import serial
 
-h1 = HuskyLensLibrary("SERIAL", "/dev/ttyUSB0", 3000000)  # Serial port PC
+h1 = HuskyLensLibrary("SERIAL", "COM7")  # Serial port PC
 # hl = HuskyLensLibrary("I2C","", address=0x32,channel=0) # I2C raspeberry pi
 print(h1.knock())
--- Camera tracking/Huskylens_Python/exampleHL.py
+++ Camera tracking/Huskylens_Python/exampleHL.py
@@ -17,142 +17,162 @@ algorthimsByteID = {
     "ALGORITHM_QR_CODE_RECOGNTITION": "0700",
     "ALGORITHM_BARCODE_RECOGNTITION": "0800",
 }
-commandList = ['knock()', 
-               'setCustomName() #Random String & Cords', 
-               'customText() #Random String & Cords', 
-               'clearText()', 
-               'requestAll()', 
-               'saveModelToSDCard(1)', 
-               'loadModelFromSDCard(1)', 
-               'savePictureToSDCard()', 
-               'count()',
-               'learnedObjCount()',
-               'saveScreenshotToSDCard()', 
-               'blocks()', 
-               'arrows()', 
-               'learned()', 
-               'learnedBlocks()', 
-               'learnedArrows()', 
-               'getObjectByID(1)', 
-               'getBlocksByID(1)', 
-               'getArrowsByID(1)', 
-               'algorthim() #Random Choice', 
-               'learn(1)', 
-               'forget()', 
-               'frameNumber()',
-               ""
-            ]
+commandList = [
+    "knock()",
+    "setCustomName() #Random String & Cords",
+    "customText() #Random String & Cords",
+    "clearText()",
+    "requestAll()",
+    "saveModelToSDCard(1)",
+    "loadModelFromSDCard(1)",
+    "savePictureToSDCard()",
+    "count()",
+    "learnedObjCount()",
+    "saveScreenshotToSDCard()",
+    "blocks()",
+    "arrows()",
+    "learned()",
+    "learnedBlocks()",
+    "learnedArrows()",
+    "getObjectByID(1)",
+    "getBlocksByID(1)",
+    "getArrowsByID(1)",
+    "algorthim() #Random Choice",
+    "learn(1)",
+    "forget()",
+    "frameNumber()",
+    "",
+]
 
 
 def printMenu():
-    print("**********************************************************"*2)
+    print("**********************************************************" * 2)
     print("MENU OPTIONS:")
-    for i in range(0, len(commandList)-1, 2):
-        part1 = chr(i+97)+"). "+commandList[i].ljust(45, " ")
-        print(part1+chr(i+1+97)+"). "+commandList[i+1]+"\t\t ")
-    print("[*] TYPE \"MENU\" to show the command menu[*]")
-    print("[*] TYPE \"QUIT\" to quit[*]")
-    print("**********************************************************"*2)
+    for i in range(0, len(commandList) - 1, 2):
+        part1 = chr(i + 97) + "). " + commandList[i].ljust(45, " ")
+        print(part1 + chr(i + 1 + 97) + "). " + commandList[i + 1] + "\t\t ")
+    print('[*] TYPE "MENU" to show the command menu[*]')
+    print('[*] TYPE "QUIT" to quit[*]')
+    print("**********************************************************" * 2)
     print("")
 
+
 def printObjectNicely(obj):
-    count=1
-    if(type(obj)==list):
+    count = 1
+    if type(obj) == list:
         for i in obj:
-            print("\t "+ ("BLOCK_" if i.type=="BLOCK" else "ARROW_")+str(count)+" : "+ json.dumps(i.__dict__))
-            count+=1
+            print(
+                "\t "
+                + ("BLOCK_" if i.type == "BLOCK" else "ARROW_")
+                + str(count)
+                + " : "
+                + json.dumps(i.__dict__)
+            )
+            count += 1
     else:
-        print("\t "+ ("BLOCK_" if obj.type=="BLOCK" else "ARROW_")+str(count)+" : "+ json.dumps(obj.__dict__))
+        print(
+            "\t "
+            + ("BLOCK_" if obj.type == "BLOCK" else "ARROW_")
+            + str(count)
+            + " : "
+            + json.dumps(obj.__dict__)
+        )
 
 
 ex = 1
 printMenu()
-while(ex == 1):
+while ex == 1:
     try:
         v = input("Enter cmd letter:")
-        if(v == "QUIT"):
+        if v == "QUIT":
             ex = 0
             print("QUITING")
             break
-        if(v == "MENU"):
+        if v == "MENU":
             printMenu()
             continue
-        print('[*] COMMAND -> '+commandList[ord(v)-97] + '[*]')
+        print("[*] COMMAND -> " + commandList[ord(v) - 97] + "[*]")
         print("[*] RESPONSE [*]")
         numEnter = v
         v = v[0].lower()
-        if(v == 'a'):
-            print("\t"+hl.knock())
+        if v == "a":
+            print("\t" + hl.knock())
 
-        elif(v == 'b'):
-            print("\t"+hl.setCustomName(
-                "test_"+str(random.randint(1, 10)), random.randint(1, 3)))
+        elif v == "b":
+            print(
+                "\t"
+                + hl.setCustomName(
+                    "test_" + str(random.randint(1, 10)), random.randint(1, 3)
+                )
+            )
 
-        elif(v == 'c'):
-            print("\t"+hl.customText(
-                "hl", random.randint(5, 300), random.randint(5, 200)))
+        elif v == "c":
+            print(
+                "\t"
+                + hl.customText("hl", random.randint(5, 300), random.randint(5, 200))
+            )
 
-        elif(v == 'd'):
-            print("\t"+hl.clearText())
+        elif v == "d":
+            print("\t" + hl.clearText())
 
-        elif(v == 'e'):
+        elif v == "e":
             printObjectNicely(hl.requestAll())
 
-        elif(v == 'f'):
-            print("\t"+hl.saveModelToSDCard(99))
+        elif v == "f":
+            print("\t" + hl.saveModelToSDCard(99))
 
-        elif(v == 'g'):
-            print("\t"+hl.loadModelFromSDCard(99))
+        elif v == "g":
+            print("\t" + hl.loadModelFromSDCard(99))
 
-        elif(v == 'h'):
-            print("\t"+hl.savePictureToSDCard())
+        elif v == "h":
+            print("\t" + hl.savePictureToSDCard())
 
-        elif(v == 'i'):
-            print("\t"+str(hl.count()))
+        elif v == "i":
+            print("\t" + str(hl.count()))
 
-        elif(v == 'j'):
-            print("\t"+str(hl.learnedObjCount()))
+        elif v == "j":
+            print("\t" + str(hl.learnedObjCount()))
 
-        elif(v == 'k'):
-            print("\t"+hl.saveScreenshotToSDCard())
+        elif v == "k":
+            print("\t" + hl.saveScreenshotToSDCard())
 
-        elif(v == 'l'):
+        elif v == "l":
             printObjectNicely(hl.blocks())
 
-        elif(v == 'm'):
+        elif v == "m":
             printObjectNicely(hl.arrows())
 
-        elif(v == 'n'):
+        elif v == "n":
             printObjectNicely(hl.learned())
 
-        elif(v == 'o'):
+        elif v == "o":
             printObjectNicely(hl.learnedBlocks())
 
-        elif(v == 'p'):
+        elif v == "p":
             printObjectNicely(hl.learnedArrows())
 
-        elif(v == 'q'):
+        elif v == "q":
             printObjectNicely(hl.getObjectByID(1))
 
-        elif(v == 'r'):
+        elif v == "r":
             printObjectNicely(hl.getBlocksByID(1))
 
-        elif(v == 's'):
+        elif v == "s":
             printObjectNicely(hl.getArrowsByID(1))
 
-        elif(v == 't'):
+        elif v == "t":
             algs = list(algorthimsByteID.keys())
             a = algs[random.randint(0, 6)]
-            print("\t"+hl.algorthim(a))
+            print("\t" + hl.algorthim(a))
 
-        elif(v == 'u'):
-            print("\t"+hl.learn(1))
+        elif v == "u":
+            print("\t" + hl.learn(1))
 
-        elif(v == 'v'):
-            print("\t"+hl.forget())
+        elif v == "v":
+            print("\t" + hl.forget())
 
-        elif(v == 'w'):
-            print("\t"+hl.frameNumber())
+        elif v == "w":
+            print("\t" + hl.frameNumber())
 
         print("")
     except KeyboardInterrupt:
@@ -165,5 +185,3 @@ while(ex == 1):
     except Exception as e:
         # General error -> just print it
         print(f"Error {e}")
-
-
