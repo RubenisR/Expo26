1eb5f3e1f38cb78e410ca5112f2812d4d22eca5d
RubenisR <r.rubeniszeleznovs@student.fontys.nl>
2023-03-27 17:25:59 +0200

    Trying now with arduino

--- Camera tracking/Huskylens_Python/Object detection.py
+++ Camera tracking/Huskylens_Python/Object detection.py
@@ -1,6 +1,9 @@
 from huskylib import HuskyLensLibrary
+from huskylib import Block
 import serial
 
 h1 = HuskyLensLibrary("SERIAL", "COM7")  # Serial port PC
 # hl = HuskyLensLibrary("I2C","", address=0x32,channel=0) # I2C raspeberry pi
-print(h1.knock())
+print("Checking if camera is working: {}".format(h1.knock()))
+# call ID from class Block
+print("ID: {}".format(Block.ID))
