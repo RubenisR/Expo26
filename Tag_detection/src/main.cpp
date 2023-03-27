6ad95b23b69910d139edd97cc1d96bfea68033ec
RubenisR <r.rubeniszeleznovs@student.fontys.nl>
2023-03-27 18:45:40 +0200

    gETTING A RESPONSE

--- Tag_detection/src/main.cpp
+++ Tag_detection/src/main.cpp
@@ -1,13 +1,50 @@
 #include <Arduino.h>
 #include "HUSKYLENS.h"
-#include "SoftwareSerial.h"
+// #include "PIDLoop.h"
+
+// PIDLoop headingLoop(120, 0, 0, false);
+HUSKYLENS huskylens; // create hsukylens object
+int ID1 = 1;         // ID of the first tag
+
+void printResult(HUSKYLENSResult result)
+{
+  Serial.println("yaas");
+}
 
 void setup()
 {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Wire.begin(); // begins comunication with the HUSKYLENS
+  while (!huskylens.begin(Wire))
+  {
+    Serial.println(F("Begin failed!"));
+    Serial.println(F("1.Please recheck the \"Protocol Type\" in HUSKYLENS (General Settings>>Protol Type>>I2C)"));
+    Serial.println(F("2.Please recheck the connection."));
+    delay(100);
+  }
+  huskylens.writeAlgorithm(ALGORITHM_TAG_RECOGNITION); // set the algorithm to tag recognition
 }
 
 void loop()
 {
-  // put your main code here, to run repeatedly:
+  // int32_t error;
+  if (!huskylens.request(ID1))
+  { // cannot request data from the HUSKYLENS
+    Serial.println("check connection to camera");
+  }
+  else if (!huskylens.isLearned()) // if object has not been learned
+  {
+    Serial.println("No Object learned");
+  }
+  else if (!huskylens.available()) // if no object is detected
+  {
+    Serial.println("No Object detected");
+  }
+  else
+  {
+    HUSKYLENSResult result = huskylens.read(); // read the result
+    printResult(result);                       // print the result
+    // error = (int32_t)result.xTarget - (int32_t)160; // Calculate the tracking error
+    // headingLoop.update(error);
+  }
 }
