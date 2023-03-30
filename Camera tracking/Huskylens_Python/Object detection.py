6a8979ab8a9ba84317d93f39b7b8976aaadcaba5
RubenisR <r.rubeniszeleznovs@student.fontys.nl>
2023-03-30 15:34:43 +0200

    trying to do this in python

--- Camera tracking/Huskylens_Python/Object detection.py
+++ Camera tracking/Huskylens_Python/Object detection.py
@@ -1,9 +1,38 @@
-from huskylib import HuskyLensLibrary
-from huskylib import Block
+from huskylib import HuskyLensLibrary as huskylens
+import numpy as np
 import serial
+import json
+import time
 
-h1 = HuskyLensLibrary("SERIAL", "COM7")  # Serial port PC
+h1 = huskylens("SERIAL", "COM7")  # Serial port PC
 # hl = HuskyLensLibrary("I2C","", address=0x32,channel=0) # I2C raspeberry pi
 print("Checking if camera is working: {}".format(h1.knock()))
-# call ID from class Block
-print("ID: {}".format(Block.ID))
+h1.algorthim("ALGORITHM_TAG_RECOGNITION")
+
+
+def printObjectNicely(obj):
+    count = 1
+    if type(obj) == list:
+        for i in obj:
+            print(
+                "\t "
+                + ("BLOCK_" if i.type == "BLOCK" else "ARROW_")
+                + str(count)
+                + " : "
+                + json.dumps(i.__dict__)
+            )
+            count += 1
+    else:
+        print(
+            "\t "
+            + ("BLOCK_" if obj.type == "BLOCK" else "ARROW_")
+            + str(count)
+            + " : "
+            + json.dumps(obj.__dict__)
+        )
+
+
+while True:
+    printObjectNicely(h1.getObjectByID(1))
+    # delay by 1 second
+    time.sleep(1)
