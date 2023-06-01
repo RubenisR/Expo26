f061dbbe0ef20dee3d49abc35602024a270b9f16
RubenisR <r.rubeniszeleznovs@student.fontys.nl>
2023-06-01 17:19:57 +0200

    update on code
    exported x,y,h and w coordinates to understand the location and distance of opject

--- Tag_detection/src/main.cpp
+++ Tag_detection/src/main.cpp
@@ -1,22 +1,42 @@
 #include <Arduino.h>
 #include "HUSKYLENS.h"
-// #include "PIDLoop.h"
 
-// PIDLoop headingLoop(120, 0, 0, false);
 HUSKYLENS huskylens; // create hsukylens object
 int ID1 = 1;         // ID of the first tag
+int x;
+int y;
+int w;
+int h;
 
 void printResult(HUSKYLENSResult result)
 {
   if (result.command == COMMAND_RETURN_BLOCK)
   {
-    Serial.println(String() + F("Block:xCenter=") + result.xCenter + F(",yCenter=") + result.yCenter + F(",width=") + result.width + F(",height=") + result.height + F(",ID=") + result.ID);
+    //Serial.println(String() + F("Block:xCenter=") + result.xCenter + F(",yCenter=") + result.yCenter + F(",width=") + result.width + F(",height=") + result.height + F(",ID=") + result.ID);
+   x=result.xCenter;
+   y=result.yCenter;
+   w=result.width;
+   h=result.height;
+
   }
   else
   {
     Serial.println("Object unknown!");
   }
   delay(1000);
+}
+
+void objectlocation(){ //if object is to the right or left then move back to center
+if (x<160) {
+  Serial.println("move left");
+}
+else if (x>160) {
+  Serial.println("move right");
+}
+else if (x==160) {
+  Serial.println("centered");
+
+}
 }
 
 void setup()
@@ -35,7 +55,6 @@ void setup()
 
 void loop()
 {
-  // int32_t error;
   if (!huskylens.request(ID1))
   { // cannot request data from the HUSKYLENS
     Serial.println("check connection to camera");
@@ -52,7 +71,13 @@ void loop()
   {
     HUSKYLENSResult result = huskylens.read(); // read the result
     printResult(result);                       // print the result
-    // error = (int32_t)result.xTarget - (int32_t)160; // Calculate the tracking error
-    // headingLoop.update(error);
   }
+  /*Serial.println(x);
+  Serial.println(y);*/
+  Serial.print("width");
+  Serial.println(w);
+  Serial.print("height");
+  Serial.println(h);
+  delay(1000);
+  //objectlocation();
 }
